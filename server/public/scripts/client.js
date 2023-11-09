113c478a1a803b78c8f204a86cbb6a69a8acd4f4
Joshua Duenow <joshjduenow@gmail.com>
2023-11-09 10:50:22 -0600

    added get and post routes...checked good

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,9 +3,40 @@ console.log( 'js' );
 function getKoalas(){
   console.log( 'in getKoalas' );
   // axios call to server to get koalas
+  axios({
+    method: 'GET',
+    url: '/koalas'
+  }).then(function (response) {
+    console.log('getKoalas() response', response.data);
+    // renderBooks(response.data);
+  }).catch(function (error) {
+    console.log('error in GET', error);
+  });
   
 } // end getKoalas
+function addKoala(event) {
+  let incKoala = {
+    name: document.getElementById('nameIn').value,
+    age: document.getElementById('ageIn').value,
+    gender: document.getElementById('genderIn').value,
+    transfer: document.getElementById('readyForTransferIn').value,
+    notes: document.getElementById('notesIn').value
+    
 
+  }
+  console.log(incKoala);
+  axios({
+    method: 'POST',
+    url: '/koalas',
+    data: incKoala,
+  }).then(function (response) {
+    console.log('incKoala()', response.data);
+    // refreshBooks();
+  }).catch(function (error) {
+    console.log('Error in POST', error)
+    alert('Unable to add Koala at this time. Please try again later.');
+  });
+}
 function saveKoala(){
   console.log( 'in saveKoala' );
   // axios call to server to get koalas
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,18 +1,43 @@
+const router = require('express').Router();
 const express = require('express');
-const koalaRouter = express.Router();
-
+// const koalaRouter = express.Router();
 // DB CONNECTION
-
+const pool = require('../modules/pool');
 
 // GET
-
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "allKoalas" ORDER BY "name";';
+    pool.query(queryText).then(result => {
+        console.log('in router.get',);
+        // Sends back the results in an object
+        res.send(result.rows);
+    })
+        .catch(error => {
+            console.log('error with koalas', error);
+            res.sendStatus(500);
+        });
+});
 
 // POST
+router.post('/', (req, res) => {
+    let newKoala = req.body;
+    console.log('Adding koala', newKoala);
 
+    let queryText = `INSERT INTO "allKoalas" ("name", "gender", "age", "transfer", "notes")
+                     VALUES ($1, $2, $3, $4, $5);`;
+    pool.query(queryText, [newKoala.name, newKoala.gender, newKoala.age, newKoala.transfer, newKoala.notes])
+        .then(result => {
+            res.sendStatus(201);
+        })
+        .catch(error => {
+            console.log('Error adding new koala', error);
+            res.sendStatus(500);
+        });
+});
 
 // PUT
 
 
 // DELETE
 
-module.exports = koalaRouter;
+module.exports = router;
